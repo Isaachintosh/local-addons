ad2b23eb6282d305231b11473c1d6a28ebc571f0
Isaachintosh <isaac.tuning@gmail.com>
2021-11-28 15:04:17 -0300

    GUI Module has been created in external module

---
+++ my_module/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "my_module",
+
+    'summary': """
+        Simple UI for managing users in my_library app""",
+
+    'description': """
+        If you need to manage books and users, you can use this UI addon to make this process easely
+    """,
+
+    'author': "Nexuz",
+    'website': "http://www.nexuz.com.br",
+    'category': 'Uncategorized',
+    'version': '12.0.1',
+    'depends': ['base'],
+    'data': [
+        'views/my_contacts.xml',
+    ],
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
