7987d5fa63f65a616d78c5c4e786704268f3d783
Isaachintosh <isaac.tuning@gmail.com>
2021-11-28 15:03:43 -0300

    return actions are add to external module

---
+++ my_library_return/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': "My Library Returns Dates",
+    'summary': "Manage return dates for books",
+    'author': "Isaachintosh",
+    'website': "http://www.nexuz.com.br",
+    'category': "Uncategorized",
+    'version': '12.0.1',
+    'depends': ['my_library'],
+    'data': [
+        'views/library_book.xml',
+    ],
+}
